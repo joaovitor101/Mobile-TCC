39688f67047046f0aa0e61f81c28a1ab1f745606
joaovitor101 <joao.kusaka27@gmail.com>
2023-11-28 15:18:20 -0300

    Versão 0.1.1

--- src/screens/grafico/index.js
+++ src/screens/grafico/index.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { BarChart } from 'react-native-chart-kit';
 import api from '../../services/api';
-import Svg, {Path, SvgUri} from 'react-native-svg';
-import { Dimensions, StyleSheet} from 'react-native';
-import { styles } from '../Home/style';
-import Wave from "react-native-waves"
+import Wave from 'react-native-waves';
+
 const App = () => {
   const [chartData, setChartData] = useState([]);
-
+  const [chartData2, setChartData2] = useState([]);
   useEffect(() => {
     fetchData();
+     fetchData2();
   }, []);
 
   const fetchData = async () => {
@@ -22,59 +21,92 @@ const App = () => {
     }
   };
 
-  const labels = chartData.map(item => item.id);
+  const fetchData2 = async () => {
+    try {
+      const response = await api.get('pam3etim/grafico/listar2.php');
+      setChartData2(response.data.resultado || []); // Inicializa com um array vazio se não houver dados
+    } catch (error) {
+      console.error('Erro ao obter os dados do listar2.php', error);
+    }
+  };
+
+  const labels = chartData.map(item => item.nome);
   const data = chartData.map(item => item.porc);
 
-  const chartConfig = {
-    
-    backgroundGradientFrom: '#fff',  // Altere aqui para a cor desejada
-    backgroundGradientTo: '#fff',    // Altere aqui para a cor desejada
+  const labels2 = chartData2.map(item => item.nome);
+  const data2 = chartData2.map(item => item.porc_umidade);
 
+
+  const chartConfig = {
+    backgroundGradientFrom: '#fff',
+    backgroundGradientTo: '#fff',
     decimalPlaces: 2,
     barPercentage: 1,
     color: (opacity = 1) => `rgba(0, 102, 0, ${opacity})`,
     style: {
       borderRadius: 16,
     },
-    
   };
 
-
-
   return (
-    
-    <View style={{ backgroundColor: '#fff', flex: 1 }}> 
-      <Text style={{ fontSize: 25, textAlign: 'center', marginTop: 90, zIndex: 1, color: 'white' }}>
-        Informações Dos Registros
-      </Text>
-      {chartData.length === 0 ? (
-        <Text>Carregando...</Text>
-      ) : (
-        <BarChart
-          style={{ marginTop: 204, position: 'absolute', zIndex: 1 }}
-          data={{
-            labels,
-            datasets: [
-              {
-                data,
-              },
-            ],
-          }}
-          width={400}
-          height={300}
-          chartConfig={chartConfig}
-          verticalLabelRotation={10}
-        />
-      )}
-      <Wave placement="top"></Wave>
-<Wave placement="top" gap={90} color="#003d66" height={200} ></Wave>
+    <ScrollView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <View style={{ paddingHorizontal: 20, paddingTop: 100 }}>
+        <Text style={{ fontSize: 25, textAlign: 'center', color: 'black', marginBottom: 10 }}>
+          Seu armazenamento de água
+        </Text>
+        <View style={{ backgroundColor: '#fff', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.5, shadowRadius: 3, elevation: 4 }}>
+          {chartData.length === 0 ? (
+            <Text style={{ padding: 20 }}>Carregando...</Text>
+          ) : (
+            <BarChart
+              style={{ marginVertical: 10 }}
+              data={{
+                labels,
+                datasets: [
+                  {
+                    data,
+                  },
+                ],
+              }}
+              width={350}
+              height={300}
+              chartConfig={chartConfig}
+              verticalLabelRotation={10}
+            />
+          )}
+        </View>
+        <Wave placement="top" />
+      </View>
 
-<Wave placement="bottom" height={10}></Wave>
-<Wave placement="bottom" gap={100} color="#003d66" height={4} ></Wave>
-    </View>
-    
+      <View style={{ paddingHorizontal: 20, paddingBottom: 50 }}>
+        <Text style={{ fontSize: 25, textAlign: 'center', color: 'black', marginTop: 30, marginBottom: 10 }}>
+          Sua umidade do solo
+        </Text>
+        <View style={{ backgroundColor: '#fff', borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.5, shadowRadius: 3, elevation: 4 }}>
+          {chartData.length === 0 ? (
+            <Text style={{ padding: 20 }}>Carregando...</Text>
+          ) : (
+            <BarChart
+              style={{ marginVertical: 10 }}
+              data={{
+                labels: labels2,
+                datasets: [
+                  {
+                    data: data2,
+                  },
+                ],
+              }}
+              width={350}
+              height={300}
+              chartConfig={chartConfig}
+              verticalLabelRotation={10}
+            />
+          )}
+        </View>
+        {/* <Wave placement="top" /> */}
+      </View>
+    </ScrollView>
   );
 };
-
 
 export default App;
